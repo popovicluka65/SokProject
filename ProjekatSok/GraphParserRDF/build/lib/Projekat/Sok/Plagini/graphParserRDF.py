abdf31b911229547a9e8540556525b78a26e0dbb
Miroslav0222 <105170901+Miroslav0222@users.noreply.github.com>
2023-12-07 18:42:20 +0100

    implemented rdf graph parsing module

--- ProjekatSok/GraphParserRDF/build/lib/Projekat/Sok/Plagini/graphParserRDF.py
+++ ProjekatSok/GraphParserRDF/build/lib/Projekat/Sok/Plagini/graphParserRDF.py
@@ -1,3 +1,11 @@
-class GraphParserRDF:
+
+from API.Projekat.Sok.Osnova.Services.graph import GraphParserBase
+from GraphParserRDF.Projekat.Sok.Plagini.graphParserRDFService import loadGraph
+class GraphParserRDF(GraphParserBase):
     def identifier(self):
         return "GrafParserRDF"
+    def name(self):
+        return "grafParserJSON"
+
+    def load(self, example_rdf_path=None):
+        return loadGraph(example_rdf_path)
