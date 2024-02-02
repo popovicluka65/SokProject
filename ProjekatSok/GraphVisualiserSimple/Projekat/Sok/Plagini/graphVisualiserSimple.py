92db76a9100cd0a6bc2e0232e71f896ad535039b
matijap59 <matijap59@gmail.com>
2024-02-02 18:34:58 +0100

    #15

--- ProjekatSok/GraphVisualiserSimple/Projekat/Sok/Plagini/graphVisualiserSimple.py
+++ ProjekatSok/GraphVisualiserSimple/Projekat/Sok/Plagini/graphVisualiserSimple.py
@@ -2,6 +2,7 @@ from Projekat.Sok.Osnova.Model.graph import Graph
 from Projekat.Sok.Osnova.Services.graph import GraphVisualiserBase
 from Projekat.Sok.Plagini.graphVisualiserSimpleService import simpleVisualize
 
+
 class GraphVisualiserSimple(GraphVisualiserBase):
     def identifier(self):
         return "GrafVisualiserSimple"
@@ -10,6 +11,11 @@ class GraphVisualiserSimple(GraphVisualiserBase):
         return "GrafVisualiserSimple"
 
     def visualize(self, graph: Graph):
-        simple_content=simpleVisualize(graph)
-        with open('simple.html', 'w', encoding="utf-8") as file:
-            file.write(simple_content)
+        # print("UDJE U FUNKCIJU")
+        # simple_content=simpleVisualize(graph)
+        # with open('simple1.html', 'w', encoding="utf-8") as file:
+        #     file.write(simple_content)
+        # print("SIMPLE CONTENT")
+        # print(simple_content)
+        # return simple_content
+        return "A"
--- README.md
+++ README.md
@@ -1,11 +1,28 @@
-SV4/2021 Luka Popovic, SV5/2021 Matija Popovic, SV43/2021 Miroslav Blagojevic, SV51/2021 Sava Janjic
-Koraci pokretanja
-python -m venv venv
-venv\Scripts\activate
-pip install jinja2
-pip install rdflib
-pip install ./API
-pip install ./Core
-opcioni plaginovi:
-./GraphParserJSON ./GraphParserRDF ./GraphVisualiserBlock ./GraphVisualiserSimple
--dodati semantiku
+
+# Sok Project
+
+Install Python packages using pip:
+
+    ```bash
+    pip install jinja2
+    pip install rdflib
+    pip install ./API
+    pip install ./Core
+    ```
+
+### Opcioni Pluginovi
+
+Additionally, you can install optional plugins based on your needs:
+
+  ```bash
+  pip install ./GraphParserJSON
+  pip install ./GraphParserRDF
+  pip install ./GraphVisualiserSimple
+  pip install ./GraphVisualiserBlock
+  ```
+
+# Members
+- SV4/2021 Luka Popovic
+- SV5/2021 Matija Popovic 
+- SV43/2021 Miroslav Blagojevic
+- SV51/2021 Sava Janjic
